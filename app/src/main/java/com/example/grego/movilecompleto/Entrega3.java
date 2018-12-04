7bcbe1b15f23ae465e329c7068f78fe35feb21e7
Fermin <dffermin@gmail.com>
2018-12-04 17:45:00 -0300

    Entrega 3 agregada

--- app/src/main/java/com/example/grego/movilecompleto/Entrega3.java
+++ app/src/main/java/com/example/grego/movilecompleto/Entrega3.java
@@ -1,13 +1,95 @@
 package com.example.grego.movilecompleto;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Entrega3 extends AppCompatActivity {
+
+    private boolean active = false;
+    private int counter = 0;
+    TextView text_time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entrega3);
+        final Button btn_start = (Button) findViewById(R.id.btn_start2);
+        final Button btn_stop = (Button) findViewById(R.id.btn_stop2);
+        final Button btn_reset = (Button) findViewById(R.id.btn_reset);
+        text_time = (TextView) findViewById(R.id.text_time);
+
+        text_time.setText("0");
+        btn_stop.setEnabled(false);
+
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn_stop.setEnabled(true);
+                btn_start.setEnabled(false);
+                active=true;
+
+                final AsyncTask<?,?,?> asyncTask = new AsyncTask<Object, Integer, Object>() {
+                    @Override
+                    protected Object doInBackground(Object... objects) {
+                        while (active) {
+                            try {
+                                synchronized ((Entrega3.this)) {
+                                    Entrega3.this.wait(1000);
+                                }
+                                if (!active)
+                                    break;
+                            }
+                            catch (InterruptedException e){
+
+                            }
+                            counter++;
+                            Log.d("hola","Van:" + counter +"seg.");
+                            publishProgress(counter);
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    protected void onProgressUpdate(Integer... values) {
+                        super.onProgressUpdate(values);
+                        text_time.setText(String.valueOf(counter));
+                    }
+
+                };
+
+                asyncTask.execute();
+            }
+        });
+
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn_stop.setEnabled(false);
+                btn_start.setEnabled(true);
+                active=false;
+                synchronized (Entrega3.this){
+                    Entrega3.this.notifyAll();
+                }
+            }
+        });
+
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn_stop.setEnabled(false);
+                btn_start.setEnabled(true);
+                text_time.setText("0");
+                active=false;
+                counter=0;
+                synchronized (Entrega3.this){
+                    Entrega3.this.notifyAll();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/grego/movilecompleto/Multiplication.java
+++ app/src/main/java/com/example/grego/movilecompleto/Multiplication.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 public class Multiplication extends AppCompatActivity {
 
     public static final String RESULTADO = "Resultado";
+    public static final String EMPTY = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +19,8 @@ public class Multiplication extends AppCompatActivity {
         Button boton_multiplicar = findViewById(R.id.buttonMultiplicar);
         final EditText num1 = (EditText) findViewById(R.id.editTextOperando1);
         final EditText num2 = (EditText) findViewById(R.id.editTextOperando2);
+        num1.setText(EMPTY);
+        num2.setText(EMPTY);
         boton_multiplicar.setOnClickListener(v -> {
             Intent replyIntent = new Intent();
             int resultado = Integer.parseInt(num1.getText().toString()) * Integer.parseInt(num2.getText().toString());
